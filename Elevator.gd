67b6169458ec3a9f0bb8a39f0374c82b09dab735
SavageMode123 <3034999@edison.k12.nj.us>
2025-05-20 12:05:30 -0400

    Stuff

---
+++ Elevator.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@export var Players: Node3D
+@onready var openButton: Node3D = get_node("OpenButton")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
