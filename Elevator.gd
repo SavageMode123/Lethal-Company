0c64400dac7f97e1ce4bc7e1a5a4f3287a3f6d81
SavageMode123 <3034999@edison.k12.nj.us>
2025-05-22 11:18:34 -0400

    Added door animation

--- Elevator.gd
+++ Elevator.gd
@@ -1,20 +1,41 @@
 extends Node3D
 
 @export var Players: Node3D
-@onready var openButton: Node3D = get_node("OpenButton")
+@onready var openButton: StaticBody3D = get_node("OpenButton")
+
+@onready var door: StaticBody3D = get_node("Door")
+@onready var doorAnimationPlayer: AnimationPlayer = door.get_node("AnimationPlayer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+var canPressOpenButton: bool = false
+var doorOpen: bool = false
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("Interact"):
+		if canPressOpenButton == true:
+			if doorOpen == false:
+				doorAnimationPlayer.play("ElevatorDoorOpen")
+			else:
+				doorAnimationPlayer.play("ElevatorDoorOpen", -1, -1, true)
+				
+			doorOpen = !doorOpen
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	for player in Players.get_children():
 		var camera: Camera3D = player.get_node("Camera")
 		var interactRay: RayCast3D = camera.get_node("InteractRay")
 		var collider: Object = interactRay.get_collider()
+		
+		var playerUI: Control = player.get_node("UI")
+		var interactLabel: Label = playerUI.get_node("Interact")
 
-		if collider:
-			print(collider)
-			
+		if collider and collider.name == "OpenButton":
+			interactLabel.visible = true
+			canPressOpenButton = true
+		else:
+			interactLabel.visible = false
+			canPressOpenButton = false
