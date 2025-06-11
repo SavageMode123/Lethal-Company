15aaf6ed415cfffe3c56680ffa415239e1aa639b
TheWhiteLionGod <aaryan12jul@gmail.com>
2025-06-11 12:36:50 -0400

    Procedural Generation v1.5
    Hallway Spawning Is Complete.
    
    Revamped Wall Deletion Resulting in Fake Rooms.
    Trade off: Algorithm is O(n^2)
    
    Added Random branching into existing hallways as well as fixed connection between rooms.

--- ProceduralGenerator.gd
+++ ProceduralGenerator.gd
@@ -5,7 +5,7 @@ extends Node3D
 @export var Root: Node3D
 
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
-var chunk_pos: Array[Vector3] = []
+var chunk_pos: Array[Vector3] = [Vector3.ZERO, Vector3(4, 0, 0), Vector3(-4, 0, 0), Vector3(0, 0, 4), Vector3(0, 0, -4)] # "Spawn Chunks"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -59,34 +59,20 @@ func spawn_hallways() -> Array[Node3D]:
 	await get_tree().create_timer(1.0).timeout # Waiting One Second Before Deleting Walls
 
 	# Deleting Walls of Hallways
-	for index in range(len(hallways)):
-		var object = hallways[index]
-		# Deleting Walls Based on Previous and Next Hallway Position
-		if index != 0:
-			var prev_pos: Vector3 = chunk_pos[index-1]
-			var dir: Vector3 = Vector3(prev_pos.x, prev_pos.y, prev_pos.z).direction_to(chunk_pos[index])
+	for i in range(len(hallways)):
+		var object = hallways[i]
+		for j in range(len(hallways)):
+			if (chunk_pos[i] - chunk_pos[j]).length() != 4:
+				continue
 
+			var dir: Vector3 = chunk_pos[i].direction_to(chunk_pos[j])
 			if dir == Vector3.BACK:
-				
-				object.get_node("Wall").call_deferred("queue_free")
-			elif dir == Vector3.FORWARD:
 				object.get_node("Wall4").call_deferred("queue_free")
-			elif dir == Vector3.LEFT:
-				object.get_node("Wall2").call_deferred("queue_free")
-			elif dir == Vector3.RIGHT:
-				object.get_node("Wall3").call_deferred("queue_free")
-
-		if index + 1 != len(chunk_pos):
-			var next_pos: Vector3 = chunk_pos[index+1]
-			var dir: Vector3 = Vector3(next_pos.x, next_pos.y, next_pos.z).direction_to(chunk_pos[index])
-
-			if dir == Vector3.BACK:
-				object.get_node("Wall").call_deferred("queue_free")
 			elif dir == Vector3.FORWARD:
-				object.get_node("Wall4").call_deferred("queue_free")
-			elif dir == Vector3.RIGHT:
-				object.get_node("Wall3").call_deferred("queue_free")
+				object.get_node("Wall").call_deferred("queue_free")
 			elif dir == Vector3.LEFT:
+				object.get_node("Wall3").call_deferred("queue_free")
+			elif dir == Vector3.RIGHT:
 				object.get_node("Wall2").call_deferred("queue_free")
 			
 	return hallways
@@ -100,28 +86,27 @@ func hallway_connector(start_pos: Vector3, end_pos: Vector3, should_rand_walk: b
 		if cur_pos == end_pos:
 			break
 
+		# Random Walk
+		if should_rand_walk:
+			cur_pos = rand_walk(cur_pos, 1)
+			continue
+		
+		# Regular Hallways
 		var endCompatiblePos: Vector3
-		if end_pos.x == cur_pos.x or end_pos.z == cur_pos.z:
-			endCompatiblePos = end_pos
-		else:
-			endCompatiblePos = Vector3(end_pos.x, end_pos.y, cur_pos.z)
+		if end_pos.x == cur_pos.x or end_pos.z == cur_pos.z: endCompatiblePos = end_pos
+		else: endCompatiblePos = Vector3(end_pos.x, end_pos.y, cur_pos.z)
+		
 		var dir: Vector3 = cur_pos.direction_to(endCompatiblePos).normalized()
+		var xInc: int = int(dir.x) * 4
+		var zInc: int = int(dir.z) * 4
 
-		# For Every Other Iteration do Random Walk
-		if (endCompatiblePos - cur_pos).length() > 8 and should_rand_walk:
-			rand_walk(cur_pos, 1, dir)
-			cur_pos = chunk_pos[-1]
-		elif not should_rand_walk:
-			var xInc: int = int(dir.x) * 4
-			var zInc: int = int(dir.z) * 4
-
-			if cur_pos + Vector3(xInc, 0, zInc) not in chunk_pos:
-				chunk_pos.append(cur_pos + Vector3(xInc, 0, zInc))
+		if cur_pos + Vector3(xInc, 0, zInc) not in chunk_pos:
+			chunk_pos.append(cur_pos + Vector3(xInc, 0, zInc))
 			
-			cur_pos = chunk_pos[-1] # Current Position Will Always Be A Valid Chunk
+			cur_pos = chunk_pos[-1]
 
 		
-func rand_walk(cur_pos: Vector3, iterations: int, last_dir: Vector3 = Vector3.FORWARD):
+func rand_walk(cur_pos: Vector3, iterations: int, last_dir: Vector3 = Vector3.FORWARD) -> Vector3:
 	for i in range(iterations):
 		var directions: Array[Vector3] = [Vector3.FORWARD, Vector3.BACK, Vector3.LEFT, Vector3.RIGHT, last_dir]
 		directions.shuffle()
@@ -132,13 +117,20 @@ func rand_walk(cur_pos: Vector3, iterations: int, last_dir: Vector3 = Vector3.FO
 
 		last_dir = cur_dir
 		chunk_pos.append(cur_pos + (4 * cur_dir))
-		cur_pos = cur_pos + (4 * cur_dir)		
+		cur_pos = cur_pos + (4 * cur_dir)
+	return cur_pos
 
 func generateMap(_origin_chunk: Vector3) -> void:
 	# Spawning Hallways
 	hallway_connector(Vector3(0, 0, 8), Vector3(128, 0, 128))
-	print(rng.randi_range(0, len(chunk_pos)-1), len(chunk_pos))
-	hallway_connector(chunk_pos[rng.randi_range(0, len(chunk_pos)-1)], Vector3.ZERO, true)
+
+	# Random Halways
+	var pointer: int = 5
+	for i in range(10):
+		hallway_connector(chunk_pos[pointer], Vector3.ZERO, true)
+		pointer += rng.randi_range(4, 8)
+		if pointer >= len(chunk_pos): pointer -= len(chunk_pos)
+
 	spawn_hallways()
 
 
